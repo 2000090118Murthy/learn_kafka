c80ad9c475709447ca2fd1510b564e393fe8e68a
unknown <murthynandhula2016@gmail.com>
2025-09-06 19:58:43 +0530

    introducing kafka to transaction service

---
+++ transaction-service/src/main/java/transaction_service/repository/TransactionRepository.java
@@ -0,0 +1,8 @@
+package transaction_service.repository;
+
+import transaction_service.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
